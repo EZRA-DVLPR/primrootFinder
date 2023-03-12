65c4d793e0e44fdb2f458638e1f34f480fd4088d
Isaiah-Martinez <82429414+Isaiah-Martinez@users.noreply.github.com>
2023-03-12 15:10:48 -0700

    outline of program

---
+++ main.py
@@ -0,0 +1,13 @@
+#Find an example where g is a Prim Root mod p:prime but not mod p^2
+#this means you would have to use g + p as the prim root
+
+#func: isPrime
+
+#func: Euler Phi Function
+#       break into primes then multiply by the PHI(prime)
+
+#func: determine if g is a primitive root
+#       if Ord_p(g) = PHI(p)
+
+#later add user input
+#func: user input grabbing
